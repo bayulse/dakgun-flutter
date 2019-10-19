b8b38d99b8107a5b913c92de9766c598989a1617
bayulse <bayulse@gmail.com>
2019-10-19 08:43:04 +0700

    Add colors

--- lib/ui/shared/app_colors.dart
+++ lib/ui/shared/app_colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color backgroundColor = Color(0xFFF6F9FC);
 const Color primaryTextColor = Color(0xFF32325D);
+const Color subtitleTextColor = Color(0xFF8898AA);
+const Color refCategoryTextColor = Color(0xFF43A689);
