43b95cd02180f8dd1cebdbe20063d36777ce80b0
bayulse <bayulse@gmail.com>
2019-10-19 08:44:03 +0700

    Add text styles

---
+++ lib/ui/shared/text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:dakgun/ui/shared/app_colors.dart';
+import 'package:flutter/material.dart';
+
+const headerStyle = TextStyle(
+    color: primaryTextColor, fontSize: 32.0, fontWeight: FontWeight.w800);
+const appBarTitleStyle = TextStyle(
+    color: primaryTextColor, fontSize: 20.0, fontWeight: FontWeight.w600);
+const subtitleStyle = TextStyle(color: subtitleTextColor, fontSize: 14.0);
+const subheadStyle = TextStyle(
+    color: primaryTextColor, fontSize: 20.0, fontWeight: FontWeight.w800);
+const userStyle = TextStyle(
+    color: primaryTextColor, fontSize: 14.0, fontWeight: FontWeight.bold);
+const titleImgStyle =
+    TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.w800);
+const titleImgSmallStyle =
+    TextStyle(color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.bold);
+const descStyle = TextStyle(color: primaryTextColor, fontSize: 14.0);
+const descSmallStyle = TextStyle(color: primaryTextColor, fontSize: 12.0);
+const refCategoryStyle = TextStyle(
+    color: refCategoryTextColor, fontSize: 10.0, fontWeight: FontWeight.w800);
